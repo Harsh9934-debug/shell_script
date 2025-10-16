6f4b41c52621abb9908cbd027605a2a3913fe040
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-10-16 20:23:22 +0530

    Updated the program

--- 12th.sh
+++ 12th.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 echo "Creating a zombie process..."
-(
-  echo "Child process $$ exiting..."
-  exit 0
-) &
 
 echo "Child PID: $child_pid"
 echo "Parent PID: $$"
+echo "Zombie sleep for the 20 second"
 sleep 20
-echo "Check zombie process using: ps -l | grep $child_pid"
+echo "Zombie process completes for the 20 sec "
