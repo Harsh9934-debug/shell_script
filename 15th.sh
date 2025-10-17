853d6ce80be07c88cf5818ee0a011cae42444db3
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-10-17 07:56:11 +0530

    Updated the program

---
+++ 15th.sh
@@ -0,0 +1,20 @@
+FILE="employee.txt"
+read -p "Enter the user ID :" id
+record=$(grep "^$id," "$FILE")
+
+name=$(echo "$record" | cut -d',' -f2)
+salary=$(echo "$record" | cut -d',' -f3)
+dept=$(echo "$record" | cut -d',' -f4)
+
+hra=$((salary * 10/100))
+da=$((salary * 5/100))
+net=$((salary+hra+da ))
+
+echo "Enployee id : $id"
+echo "Employee name : $name"
+echo "Department   : $dept"
+echo "Basic Salary : ₹$salary"
+echo "HRA (10%)    : ₹$hra"
+echo "DA (5%)      : ₹$da"
+echo "Net Salary   : ₹$net"
+echo "--------------------------------------"
